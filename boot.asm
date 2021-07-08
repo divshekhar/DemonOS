8e29cc2e17ff95ce31d8ce6d5bdf7fae32db0b8a
divshekhar <imdshekhar@gmail.com>
2021-07-08 11:27:29 +0530

    switch to protected mode

--- boot.asm
+++ boot.asm
@@ -1,67 +1,86 @@
-ORG 0
+ORG 0x7c00
 BITS 16
 
-_start:
-    jmp short start     ; short jump (BIOS Parameter Block)
-    nop                 ; nop (NO OPERATION)
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
-times 33 db 0           ; Fills 33 bytes by 0 after short jump (BIOS Parameter Block)
+_start:
+    jmp short start                     ; short jump (BIOS Parameter Block)
+    nop                                 ; nop (NO OPERATION)
+
+times 33 db 0                           ; Fills 33 bytes by 0 after short jump (BIOS Parameter Block)
 
 start:
-    jmp 0x7c0:step2     ; code segment set to 0x7c0
+    jmp 0:step2                         ; code segment set to 0x7c0
 
 step2:
-    cli                 ; clear interrupts
-
-    mov ax, 0x7c0
-    mov ds, ax          ; data segment set to 0x7c0
-    mov es, ax          ; extra segment set to 0x7c0
+    cli                                 ; clear interrupts
 
     mov ax, 0x00
-    mov ss, ax          ; stack segment set to 0x00
-    mov sp, 0x7c00      ; stack pointer set to 0x7c00
+    mov ds, ax                          ; data segment set to 0x00
+    mov es, ax                          ; extra segment set to 0x00
+    mov ss, ax                          ; stack segment set to 0x00
+    mov sp, 0x7c00                      ; stack pointer set to 0x7c00
 
-    sti                 ; enable interrupts
+    sti                                 ; enable interrupts
 
-    mov ah, 2           ; Read Sector Command
-    mov al, 1           ; One Sector to read
-    mov ch, 0           ; Cylinder low eight bits
-    mov cl, 2           ; Read Sector two
-    mov dh, 0           ; Head number
-    mov bx, buffer
-    int 0x13            ; Hard Drive interrupt (Invoke read command)
-    jc error            ; If the carry flag is set, jump to error label
-
-    mov si, buffer
-    call print
     
+
+.load_protected:
+    cli
+    lgdt [gdt_descriptor]               ; load global descriptor table
+
+    mov eax, cr0
+    mov eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+
+; GDT
+gdt_start:
+
+gtd_null:
+    dd 0x0
+    dd 0x0
+
+
+; Offset 0x8
+gdt_code:                               ; CS should point to this
+    dw 0xffff                           ; Segment limit first 0-15 bits
+    dw 0                                ; Base first 0-15 bits
+    db 0                                ; Base 16-23 bits
+    db 0x9a                             ; Access bytes
+    db 11001111b                        ; High 4 bit flags and the low 4 bit flags
+    db 0                                ; Base 24-31 bits
+
+
+; Offset 0x10
+gdt_data:                               ; DS, SS, ES, FS, GS
+    dw 0xffff                           ; Segment limit first 0-15 bits
+    dw 0                                ; Base first 0-15 bits
+    db 0                                ; Base 16-23 bits
+    db 0x92                             ; Access bytes
+    db 11001111b                        ; High 4 bit flags and the low 4 bit flags
+    db 0                                ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start -1;
+    dd gdt_start
+
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-print:
-    mov bx, 0
-    .loop:
-        lodsb
-        cmp al, 0
-        je .done
-        call print_char
-        jmp .loop
-    .done:
-        ret
-
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db "Failed to load sector", 0
 
 times 510 - ($-$$) db 0
 dw 0xAA55
-
-buffer:
